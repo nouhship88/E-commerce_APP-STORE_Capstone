9b7b89caba80b9653690dbab8478ee7fba2a7c4e
Noah <nouhship88@gmail.com>
2023-07-01 01:42:22 -0400

    Update about page and add connect details

--- app/src/main/java/com/example/Noah_AppDev_Ecommerce/About.java
+++ app/src/main/java/com/example/Noah_AppDev_Ecommerce/About.java
@@ -3,18 +3,20 @@ package com.example.Noah_AppDev_Ecommerce;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class About extends AppCompatActivity {
+    MediaPlayer mySong;
     Button ld,gt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-
+        mySong=MediaPlayer.create(About.this,R.raw.follow);
         gt=findViewById(R.id.gt);
         ld=findViewById(R.id.ld);
 
@@ -38,5 +40,15 @@ public class About extends AppCompatActivity {
         });
 
 
+    }
+
+    public void playIT(View v){
+    mySong.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mySong.release();
     }
 }
