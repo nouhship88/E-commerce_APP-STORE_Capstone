d1e01f39ee7068442d1255202e4c2a08dc635af3
Noah <nouhship88@gmail.com>
2023-07-07 19:09:04 -0400

    Testing

--- app/src/test/java/com/example/Noah_AppDev_Ecommerce/LoginValidator.java
+++ app/src/test/java/com/example/Noah_AppDev_Ecommerce/LoginValidator.java
@@ -1,4 +1,10 @@
 package com.example.Noah_AppDev_Ecommerce;
 
 public class LoginValidator {
+
+    public static boolean validateInput(String name, String Phone, String password){
+
+        return !(name.equals(null) || Phone.isEmpty());
+    }
+
 }
